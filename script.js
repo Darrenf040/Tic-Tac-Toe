dc8e765c57166f37ca1abe4376dccbeaeeb33f1b
Darrenf040 <darrenfr83@gmail.com>
2023-08-30 22:31:47 -0500

    Module pattern for gameBoard with 2 objects

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const gameBoard = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
-console.log(gameBoard);
+// const gameBoard = [
+//     ['', '', ''],
+//     ['', '', ''],
+//     ['', '', '']
+// ];
+// console.log(gameBoard);
 
-const fillBoard = gameBoard.map((row) => {
-    return row.map(cell => {
-        return cell = "x";
-    });
-});
-console.log(fillBoard);
+// const fillBoard = gameBoard.map((row) => {
+//     return row.map(cell => {
+//         return cell = "x";
+//     });
+// });
+// console.log(fillBoard);
+
+const gameBoard = (() => {
+    const empty = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    const add = empty.map(row => {
+        return row.map(cell => {
+            return cell = "x";
+        })
+    })
+    return {empty,add};
+})();
+console.log(gameBoard.empty)
+console.log(gameBoard.add);
