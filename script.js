fc67fb11edb257848e28a3192efd896d132589e3
Darrenf040 <darrenfr83@gmail.com>
2023-09-08 17:25:49 -0500

    new page with empty board

--- script.js
+++ script.js
@@ -12,18 +12,17 @@
 // });
 // console.log(fillBoard);
 
+const body = document.querySelector("body");
 const gameBoard = (() => {
     const empty = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
     ];
-    const add = empty.map(row => {
-        return row.map(cell => {
-            return cell = "x";
-        })
-    })
-    return {empty,add};
+    const create = empty.forEach(row => row.forEach(cell =>{
+        boardCell = document.createElement("div")
+        boardCell.className = "cell";
+        body.appendChild(boardCell);
+    }))
+    return {empty, create};
 })();
-console.log(gameBoard.empty)
-console.log(gameBoard.add);
