ecacdfd7d0344ba60a2b0faf220a90e9f6cbba29
Darrenf040 <darrenfr83@gmail.com>
2023-09-09 12:20:19 -0500

    testing ai marker positioning

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const gameBoard = (() => {
     const board = document.createElement("div");
     board.className = "board";
     body.appendChild(board);
+    let i = 0;
     const empty = [
         ['', '', ''],
         ['', '', ''],
@@ -28,16 +29,45 @@ const gameBoard = (() => {
             boardCell.className = `cell row${rowIndex + 1} col${cellIndex + 1}`;
             board.appendChild(boardCell);
             boardCell.addEventListener("click", e => {
-                e.target.textContent = playerMarker;
+                if(e.target.textContent == ""){
+                    e.target.textContent = playerMarker;
+                    i++;
+                }
             })
         })
     })
     const playerMarker = prompt("x or o");
-    
-    return {create, playerMarker};
+    const c = document.querySelectorAll(".row2");
+    const p = document.querySelectorAll(".row3");
+    c.forEach(cell => {
+        cell.textContent = "x";
+    })
+    p.forEach(cell => {
+        cell.textContent = "x";
+    })
+
+    return {create, playerMarker, i};
 })();
 const player = (name="player") => {
     return {name};
 }
 function playGame(){
+    const chooseTurn = [true, false];
+    let playerTurn = false;
+    if(playerTurn == false){
+        let randomCell;
+        do{
+            const rowNumber = Math.floor(1 + Math.random () * 3);
+            const colNumber = Math.floor(1 + Math.random () * 3);    
+            randomCell = document.querySelector(`.row${rowNumber}.col${colNumber}`)
+        }while(randomCell.textContent != "")
+        if(gameBoard.playerMarker == "x"){
+            randomCell.textContent = "o";
+        }
+        else{
+            randomCell.textContent = "x";
+        }
+    }
+
 }
+playGame();
