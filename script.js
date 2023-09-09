e37456d410fbfad733aaad8bdce5751cf54f9239
Darrenf040 <darrenfr83@gmail.com>
2023-09-09 00:12:47 -0500

    player choose marker & add marker to board

--- script.js
+++ script.js
@@ -14,15 +14,34 @@
 
 const body = document.querySelector("body");
 const gameBoard = (() => {
+    const board = document.createElement("div");
+    board.className = "board";
+    body.appendChild(board);
     const empty = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
     ];
-    const create = empty.forEach(row => row.forEach(cell =>{
-        boardCell = document.createElement("div")
-        boardCell.className = "cell";
-        body.appendChild(boardCell);
-    }))
-    return {empty, create};
+    const create = empty.forEach((row, rowIndex)=> {
+        row.forEach((cell, cellIndex) => {
+            const boardCell = document.createElement("div");
+            boardCell.className = `cell row${rowIndex + 1} col${cellIndex + 1}`;
+            board.appendChild(boardCell);
+        })
+    })
+    const add = board.addEventListener("click", e => {
+        const p = document.createElement("p")
+        p.className = "marker"
+        p.textContent = playerMarker
+        e.target.appendChild(p)
+        // e.target.textContent = playerMarker;
+    })
+    const playerMarker = prompt("x or o");
+    
+    return {create, add, playerMarker};
 })();
+const player = (name="player") => {
+    return {name};
+}
+function playGame(){
+}
